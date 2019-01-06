5b8b22ebf668a49450a675f2f52ddfcf7ef76be0
wenniok <wenniokfo@gmail.com>
2019-01-05 22:37:25 -0400

    First project Kotlin

---
+++ Sorteio/app/src/main/java/com/example/wennio/sorteio/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.wennio.sorteio
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun play(view: View){
+        var result = findViewById<View>(R.id.result) as TextView
+        var numberDrawn = Random().nextInt(11)
+        result.setText("Numero sorteado e: $numberDrawn")
+    }
+}
